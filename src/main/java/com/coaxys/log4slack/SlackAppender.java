6a239862fad2162095027237938b3c64536f3b0f
kevin moreau <kevin.moreau@coaxys.com>
2016-04-10 22:41:54 +0200

    feat: Add asynchronous request for the slack log sending

--- src/main/java/com/coaxys/log4slack/SlackAppender.java
+++ src/main/java/com/coaxys/log4slack/SlackAppender.java
@@ -2,14 +2,13 @@ package com.coaxys.log4slack;
 
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.log4j.AppenderSkeleton;
-import org.apache.log4j.Layout;
 import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.concurrent.Future;
 
 public class SlackAppender extends AppenderSkeleton {
 
@@ -18,16 +17,24 @@ public class SlackAppender extends AppenderSkeleton {
     @Override
     protected void append(LoggingEvent event) {
         if (url != null && url.length() > 0 && event.getLevel() == Level.ERROR) {
-            try {
-                HttpResponse<String> response = Unirest.post(url)
-                        .header("accept", "application/json")
-                        .body(LoggingEventSlackifier.slackinify(event))
-                        .asString();
-                String statusText = response.getStatusText();
-                System.out.println(statusText);
-            } catch (UnirestException e) {
-                e.printStackTrace();
-            }
+            Future<HttpResponse<String>> future = Unirest.post(url)
+                    .header("accept", "application/json")
+                    .body(LoggingEventSlackifier.slackinify(event))
+                    .asStringAsync(new Callback<String>() {
+
+                        public void failed(UnirestException e) {
+                            System.out.println("The slack request has failed");
+                        }
+
+                        public void completed(HttpResponse<String> response) {
+                            System.out.println(response.getStatusText());
+                        }
+
+                        public void cancelled() {
+                            System.out.println("The slack request has been cancelled");
+                        }
+
+                    });
         }
     }
 
